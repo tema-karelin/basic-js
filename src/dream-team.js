ee61196f2ec2f2fd1d01a7a12d9a837f9d429002
Artem Karelin <Artem.Karelin@ibm.com>
2022-04-29 22:49:23 +0300

    feat: dream team task solved

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(arr) {
+
+  // проверка массив ли в аргументе 
+  if (arr == 0 || !Array.isArray(arr)) return false;
+
+  // создать массив, все элементы которого - строки
+  let onlyStringArr = [];
+  arr.forEach(element => {
+    // проверка, строка ли
+    if (typeof element == 'string') {
+      // ~ trim ~
+      //^ если строка, то убрать пробелы в начале и конце строки
+      // ~ charAt(0) ~ toUpperCase()
+      //^ взять только первый символ (0) и преобразовать в заглавную 
+      // ~ push ~
+      //^ добавить к массиву
+      onlyStringArr.push(element.trim().charAt(0).toUpperCase());
+    }
+  });
+  // отфильтровать массив
+  return onlyStringArr.sort(function (a, b) {
+    if (a > b) {
+      return 1;
+    }
+    if (a < b) {
+      return -1;
+    }
+    // a = b
+    return 0;
+  }).join('');
+
+  
+
+  // return arr.map(el => {
+  //   return el.charAt(0).toUpperCase().sort(el2 => 
+  //     );
+  // });
 }
 
 module.exports = {
