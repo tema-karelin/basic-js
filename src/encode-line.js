1bce3732c9abaaa31faf405fa5df484263950649
Artem Karelin <Artem.Karelin1@ibm.com>
2023-04-17 15:08:40 +0200

    feat: encode-line task is ready

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  console.log('=============\n new string: ', str);
+  let strArr = str.split('');
+  return strArr.reduce((acc, el) => {
+    if (acc.slice(-1) === el) {
+      //console.log('reduce is equal: ', acc + el)
+      if (!!Number(acc.slice(-2, -1))) {
+        console.log('reduce is Number: ', acc.slice(0, -2) + (Number(acc.slice(-2, -1)) + 1) + el);
+        return acc.slice(0, -2) + (Number(acc.slice(-2, -1)) + 1) + el;
+      } else {
+        console.log('reduce is Letter: ', acc.slice(0, -1) + '2' + el)
+        return acc.slice(0, -1) + '2' + el;
+      }
+    } else {
+      console.log('reduce not equal: ', acc + el);
+      return acc + el;
+    }
+  }, '')
 }
 
 module.exports = {
